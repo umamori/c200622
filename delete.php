7243fb73c59ffe6acdd1521457ae09796f3ee788
umamori <93758041+umamori@users.noreply.github.com>
2022-01-28 09:56:25 +0900

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+require('functions.php');
+
+if (isset($_SESSION['id'])) {
+	$id = $_GET['id'];
+	
+	// 投稿をチェック
+    $dbh = db_conn();
+    $sql = 'SELECT * FROM posts WHERE id=:id';
+    $messages = $dbh->prepare($sql);
+/* 期末課題　５）バインド処理を追加する */
+	 $messages->bindValue(':id', $id, PDO::PARAM_INT);
+    $messages->execute();
+	$message = $messages->fetch();
+
+	if ($message['member_id'] == $_SESSION['id']) {
+		// 削除する
+        $sql = 'DELETE FROM posts WHERE id=:id';
+        $del = $dbh->prepare($sql);
+        $del->bindValue(':id', $id, PDO::PARAM_INT);
+        $del->execute();
+	}
+}
+
+header('Location: index2.php');
+exit();
+?>
